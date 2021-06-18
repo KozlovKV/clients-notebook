c47b80c94e588dee9bc3c9a035f5854f685c3f83
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2021-06-19 01:40:22 +0600

    Added ServiceNote model

--- apps/services_app/admin.py
+++ apps/services_app/admin.py
@@ -3,3 +3,4 @@ import apps.services_app.models as services_models
 
 
 admin.site.register(services_models.Service)
+admin.site.register(services_models.ServiceNote)
--- apps/services_app/models.py
+++ apps/services_app/models.py
@@ -6,3 +6,36 @@ class Service(models.Model):
     provider = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     label = models.CharField(max_length=255)
     description = models.TextField()
+
+    def __str__(self):
+        return f'{self.label} by {self.provider}'
+
+
+class ServiceNote(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.DO_NOTHING)
+    client = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    date = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+
+    EMPTY = 0
+    OCCUPIED = 1
+    ENDED = 2
+    CANCELED = 3
+    STATUS_CHOICES = (
+        (EMPTY, 'Свободно'),
+        (OCCUPIED, 'Заято'),
+        (ENDED, 'Прошло'),
+        (CANCELED, 'Отменено'),
+    )
+    status = models.IntegerField(default=EMPTY, choices=STATUS_CHOICES)
+
+    @property
+    def status_name(self):
+        for status_pair in self.STATUS_CHOICES:
+            if status_pair[0] == self.status:
+                return status_pair[1]
+
+    def __str__(self):
+        return f'{self.service} for {self.client} at {self.date}, {self.time_start}-{self.time_end} ' \
+               f'({self.status_name})'
