82fc350511a9103749bcb41dbebbd6fc31379d6d
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2021-06-29 00:03:16 +0600

    Maked simple info view on oage of one service

--- apps/services_app/views.py
+++ apps/services_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from  django.views.generic import list as list_views
+from  django.views.generic import list as generic_list_views
+from  django.views.generic import detail as generic_detail_views
 
 from apps.front_app.views import BaseViewWithMenu
 
 from apps.services_app import models as service_models
 
 
-class ServicesListView(list_views.ListView, BaseViewWithMenu):
+class ServicesListView(generic_list_views.ListView, BaseViewWithMenu):
     object_list = []
     model = service_models.Service
     template_name = 'services_list.html'
@@ -16,8 +17,14 @@ class ServicesListView(list_views.ListView, BaseViewWithMenu):
         return super(ServicesListView, self).get(request, *args, **kwargs)
 
 
-class OneServiceCalendarView(BaseViewWithMenu):
+class OneServiceCalendarView(BaseViewWithMenu, generic_detail_views.DetailView):
     template_name = 'one_service_calendar.html'
+    object = None
+    model = service_models.Service
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(OneServiceCalendarView, self).get(request, *args, **kwargs)
 
 
 class OneServiceDayView(BaseViewWithMenu):
