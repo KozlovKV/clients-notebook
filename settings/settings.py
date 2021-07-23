6ac8f23dc54b4b6825c2bebcd011e4f8404dd365
KozlovKV <kozlov.kirill.v.work@gmail.com>
2021-07-23 15:46:55 +0600

    Changed secret key and removed its from repo

--- settings/settings.py
+++ settings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s$1j#!*_@u&)tk11&&3p)zc%*mb+k=5jgaecgdq)2o$pe=wq2t'
+SECRET_KEY = secret_const.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
